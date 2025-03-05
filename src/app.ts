985b95aea0b610e8e63e785b4e71e5f312f67da8
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-05 15:49:04 -0100

    Trying to get it to host on render

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { categories, questions } from "./api/index.ts";
+import process from "node:process";
 
 const app = new Hono();
 
 app.route("/", categories);
 app.route("/", questions);
 
+const port = process.env.PORT || 3000;
+
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
