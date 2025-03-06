cce187ad9b54e886f308cabaa9c49b7a662e9ae2
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-06 14:05:57 -0100

    adas

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
-  transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
-  },
+  testEnvironment: "jest-environment-node",
+  transform: {},
+  rootDir: "dist",
 };
