93d6f6873ebeebb80c8658b0c9387cf468a35196
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-06 13:39:29 -0100

    fuck you again

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,29 @@
 import { Hono } from "hono";
 import { categories, questions } from "./api/index.js";
 import process from "node:process";
+import { serve } from "@hono/node-server";
 
 const app = new Hono();
 
 app.route("/", categories);
 app.route("/", questions);
 
-const port = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 3000;
+const HOST = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
 
-export default {
-	port,
-	fetch: app.fetch,
-};
+serve(
+  {
+    fetch: app.fetch,
+    port: PORT,
+    hostname: HOST,
+  },
+  () => {
+    console.log(
+      `Server running on ${
+        process.env.NODE_ENV === "production"
+          ? "https://Vef2-verk3.onrender.com"
+          : `http://${HOST}:${PORT}`
+      }`,
+    );
+  },
+);
