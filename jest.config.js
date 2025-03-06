ce32632eee0c2005ee74f4ab424d31cd58a9aa57
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-06 14:02:16 -0100

    no

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,13 @@
 export default {
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
   transform: {
-    "^.+\.tsx?$": ["ts-jest", {}],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
 };
