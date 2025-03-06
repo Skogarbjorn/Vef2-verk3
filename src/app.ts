d60b47225a4dfa0d0c29b7bea7b0c8a4eaec4e11
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-06 12:42:01 -0100

    dont think this fixed jest but maybe

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import { Hono } from "hono";
 import { categories, questions } from "./api/index.js";
-import { handle } from "jsr:@hono/hono/netlify";
+import process from "node:process";
 
 const app = new Hono();
 
 app.route("/", categories);
 app.route("/", questions);
 
-export default handle(app);
+const port = Number(process.env.PORT) || 3000;
+
+export default {
+	port,
+	fetch: app.fetch,
+};
