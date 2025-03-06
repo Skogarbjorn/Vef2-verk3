63d423408017ffee70b63792f67cb334b1a98c66
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-03-06 13:16:29 -0100

    node version

--- eslint.config.js
+++ eslint.config.js
@@ -5,20 +5,20 @@ import tsParser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["src/**/*.ts"],
-    languageOptions: {
-      parser: tsParser,
-      globals: globals.browser,
-    },
-    plugins: { "@typescript-eslint": tsPlugin },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-      ],
-    },
-  },
-  pluginJs.configs.recommended,
+	{
+		files: ["src/**/*.ts"],
+		languageOptions: {
+			parser: tsParser,
+			globals: globals.browser,
+		},
+		plugins: { "@typescript-eslint": tsPlugin },
+		rules: {
+			...tsPlugin.configs.recommended.rules,
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+			],
+		},
+	},
+	pluginJs.configs.recommended,
 ];
